95f74cc41fcdc4a8650d15fda4cfddef47b1f060
eeaaea1 <ttrsea@gmail.com>
2026-07-04 22:18:36 +0300

    Add Playwright script to open ISpark page

--- check.py
+++ check.py
@@ -1,1 +1,12 @@
+from playwright.sync_api import sync_playwright
 
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+
+    page = browser.new_page()
+
+    page.goto("https://ispark.istanbul/abone/", wait_until="networkidle")
+
+    print("İSPARK sayfası başarıyla açıldı.")
+
+    browser.close()
