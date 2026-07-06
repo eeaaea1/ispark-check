c7f3d08a6b4f360843b48f45243cd333816435a7
eeaaea1 <ttrsea@gmail.com>
2026-07-06 09:58:22 +0300

    Refactor check.py to create a text file instead of using Playwright
    Removed Playwright browser automation and added file creation.

--- check.py
+++ check.py
@@ -1,32 +1,7 @@
-from playwright.sync_api import sync_playwright
+from pathlib import Path
 
 print("Program başladı")
 
-with sync_playwright() as p:
+Path("test.txt").write_text("GitHub Actions çalışıyor.", encoding="utf-8")
 
-    browser = p.chromium.launch(
-        headless=True
-    )
-
-    page = browser.new_page(
-        viewport={"width": 1400, "height": 2500}
-    )
-
-    page.goto(
-        "https://ispark.istanbul/abone/",
-        wait_until="domcontentloaded",
-        timeout=60000
-    )
-
-    page.wait_for_timeout(5000)
-
-    page.screenshot(
-        path="ispark.png",
-        full_page=True
-    )
-
-    print("Ekran görüntüsü oluşturuldu")
-
-    browser.close()
-
-print("Program bitti")
+print("test.txt oluşturuldu")
