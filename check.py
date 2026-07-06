9f8292a3cd5c84d177ac29813f4a7e668fd8eb2d
eeaaea1 <ttrsea@gmail.com>
2026-07-06 08:25:16 +0300

    Modify page navigation and add debug print statements
    Updated the URL for page navigation and added print statements for better tracking.

--- check.py
+++ check.py
@@ -1,14 +1,24 @@
 from playwright.sync_api import sync_playwright
 
+print("1- Program başladı")
+
 with sync_playwright() as p:
+    print("2- Playwright açıldı")
+
     browser = p.chromium.launch(headless=True)
 
     page = browser.new_page()
 
-    page.goto("https://ispark.istanbul/abone/", wait_until="networkidle")
+    print("3- Sayfaya gidiliyor")
 
-    page.screenshot(path="ispark.png", full_page=True)
+    page.goto("https://example.com")
 
-    print("Ekran görüntüsü alındı.")
+    print("4- Sayfa açıldı")
+
+    page.screenshot(path="ispark.png")
+
+    print("5- Ekran görüntüsü alındı")
 
     browser.close()
+
+print("6- Program bitti")
